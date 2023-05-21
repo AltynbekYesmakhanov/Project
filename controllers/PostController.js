34335c83a9d6fc3b78523123d6bea5f765108ea5
Altynbek <a.yesmakhanov03@gmail.com>
2023-05-21 12:49:39 +0600

    index.js

---
+++ controllers/PostController.js
@@ -0,0 +1,141 @@
+import PostModel from '../models/Post.js';
+
+export const getAll = async (req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec();
+
+        res.json(posts);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось получить статьи',
+        });
+    };
+};
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        
+    //     PostModel.findOneAndUpdate(
+    //     {
+    //         _id: postId,
+    //     }, 
+    //     {
+    //         $inc: { viewsCount: 1 },
+    //     },
+    //     {
+    //         returnDocument: 'after',
+    //     },
+    //     (err, doc) => {
+    //         if(err) {
+    //             console.log(err);
+    //             return res.status(500).json({
+    //                 message: 'Не удалось вернуть статью',
+    //                 });
+    //             } 
+
+    //         if(!doc) {
+    //             return res.status(404).json({
+    //                 message: 'Статья не найдена',
+    //             });
+    //         }
+            
+    //         res.json(doc);
+    //     },
+    // );
+    PostModel.findOneAndUpdate(
+        { _id: postId },
+        { $inc: { viewsCount: 1 } },
+        { returnDocument: 'after' }
+    )
+        .then(doc => {
+            if (!doc) {
+                return res.status(404).json({
+                    message: 'Статья не найдена',
+                });
+            }
+            res.json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Не удалось вернуть статью',
+            });
+        });    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось получить статьи',
+        });
+    };
+};
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        // PostModel.findOneAndDelete({
+        //     _id: postId,
+        // }, (err,doc)=>{
+        //     if (err){
+        //         console.log(err);
+        //         return res. status(500).json({
+        //             message: 'Не удалось удалить статью'
+        //         });
+        //     };
+        // });
+        PostModel.findOneAndDelete({ _id: postId })
+    .then(doc => {
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Статья не найдена',
+            });
+        }
+        // Handle the deleted document
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось удалить статью',
+        });
+    });
+
+        
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Статья не найдена',
+            });
+        };
+
+        res.json({
+            success: true,
+        })
+    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось получить статьи',
+        });
+    };
+};
+
+export const create = async (req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,
+        });
+
+        const post = await doc.save();
+
+        res.json(post);
+
+    } catch(err) {
+        console.log(err);
+      res.status(500).json({
+          message: 'Не удалось создать статью'
+      });
+    };
+};
